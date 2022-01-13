8e4a184e0ed7f5acb85ced8338144d5893e15228
konrad <svahnkon@arcada.fi>
2022-01-13 21:10:37 +0200

    added a initiative stat

--- dsoa22-battle-game-konrad-svahn/src/main/java/GameCharacter.java
+++ dsoa22-battle-game-konrad-svahn/src/main/java/GameCharacter.java
@@ -3,11 +3,23 @@ public class GameCharacter {
     private String name; 
     private int helth;
     private int attackPower;
+    private int initiative;
+    private boolean isPlayer;
 
-    public GameCharacter(String name, int helth , int attackPower) {
+    public GameCharacter(String name, int helth , int attackPower ,int initiative ,boolean isPlayer) {
         this.name = name;
         this.helth = helth;
         this.attackPower = attackPower;
+
+        if (isPlayer == true){initiative = 5;}
+        if (initiative < 1 || initiative > 9){initiative = 1;}
+        if (initiative == 5 && isPlayer == false){initiative = 4;}
+        this.initiative = initiative;
+        this.isPlayer = isPlayer;
+    }
+
+    public void takeDamage(int damage){
+        this.helth = this.helth - damage;
     }
 
     public String getName() {
@@ -26,16 +38,31 @@ public class GameCharacter {
         this.helth = helth;
     } 
 
-    public void takeDamage(int damage){
-        this.helth = this.helth - damage;
-    }
-
     public int getAttackPower() {
         return attackPower;
     }
 
     public void setAttackPower(int attackPower) {
         this.attackPower = attackPower;
+    }
+
+    public int getInitiative() {
+        return initiative;
+    }
+
+    public void setInitiative(int initiative) {
+        if (this.isPlayer == true){initiative = 5;}
+        if (initiative < 1 || initiative > 9){initiative = 1;}
+        if (initiative == 5 && this.isPlayer == false){initiative = 4;}
+        this.initiative = initiative;
+    }
+
+    public boolean isPlayer() {
+        return isPlayer;
+    }
+
+    public void setPlayer(boolean isPlayer) {
+        this.isPlayer = isPlayer;
     }
 
 }
--- dsoa22-battle-game-konrad-svahn/src/main/java/Main.java
+++ dsoa22-battle-game-konrad-svahn/src/main/java/Main.java
@@ -1,26 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
-
+    
     public static void main(String[] args) {
-          System.out.println("Game start");
-          System.out.println("A black knight blocks the bridge before you");
-          System.out.println("A battle has begun");
 
-          GameCharacter player1 = new GameCharacter("player1",100,25);
-          GameCharacter enemy1 = new GameCharacter("the black knight", 500,25);
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        
+            
+        System.out.println("Enter your name");
 
-          System.out.println(enemy1.getName()+" has "+enemy1.getHelth()+" health");
-          System.out.println(player1.getName()+" has "+player1.getHelth()+" health");
+        name = scanner.nextLine();
+        GameCharacter player1 = new GameCharacter(name,100,25,5,true);
 
-          while (true) {
+        System.out.println("Game start");
+        System.out.println("A black knight blocks the bridge before you");
+        
+
+            
+        GameCharacter enemy1 = new GameCharacter("the black knight", 500,25,4,false);
+
+        GameCharacter[] battle1 = {enemy1,player1};
+        sortCharacters(battle1);
+        printBattleStart(battle1);
+        
+
+        while (true) {
             enemy1.takeDamage(player1.getAttackPower());
             printDamage(player1,enemy1);
-            
+                
             if (enemy1.getHelth() <= 0){
                 System.out.println(player1.getName()+" has won the battle");
                 System.out.println("satisfied with your vicktory, you cross the bridge");
                 break;
             } 
-            
+                
             player1.takeDamage(enemy1.getAttackPower());
             printDamage(enemy1,player1);
 
@@ -29,15 +43,42 @@ public class Main {
                 System.out.println("you died");
                 break;
             }
-        }
+        }    
+    }
 
-          
+    public static GameCharacter[] sortCharacters(GameCharacter[] charackters){
+        for(int i = 0; i < charackters.length; i++){
+            int bigest = 0;
+            for(int j = 0 + i; j < charackters.length; j++){
+                if(j == 0 + i){bigest = j;}
+                else if(charackters[j].getInitiative() > charackters[j-1].getInitiative()){bigest = j;}
+            }
+            //System.out.println(charackters[0].getInitiative()+""+charackters[1].getInitiative()+""+charackters[2].getInitiative()+""+charackters[3].getInitiative());
+            GameCharacter temp = charackters[i];
+            charackters[i] = charackters[bigest];
+            charackters[bigest] = temp;
+        }
+        return charackters;
     }
 
     public static void printDamage(GameCharacter attacker, GameCharacter deffender){
         System.out.println(
-            deffender.getName()+" recived "+attacker.getAttackPower()+" damage. "+deffender.getName()+" now has "+deffender.getHelth()+" Health"
+            attacker.getName()+" hits "+deffender.getName()+" for "+attacker.getAttackPower()+" damage. "+deffender.getName()+" now has "+deffender.getHelth()+" Health"
         ); 
+    }
+
+    public static void printRegen(){
+        System.out.println("troll");
+    }
+
+    public static void printBattleStart(GameCharacter[] charackters){
+        System.out.println("A battle has begun");
+        for(int i = 0; i < charackters.length; i++){
+            int j = i+1;
+            System.out.println(
+                charackters[i].getName()+" has "+charackters[i].getHelth()+" helth and is number "+j+" in the atack order"
+            );
+        }
     }
 
 }
